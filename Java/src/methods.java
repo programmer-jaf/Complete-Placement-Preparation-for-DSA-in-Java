7aa7df16daee186cf718d931665bf728b3cce6e9
Programmer-Jaf <programmerjaf@gmail.com>
2024-12-07 15:52:03 +0600

    Basics of Methods

---
+++ Java/src/methods.java
@@ -0,0 +1,13 @@
+public class methods {
+  //*  create a method
+  static void sayHey(){
+    System.out.println("👋, It's programmer-Jaf");
+  }
+  public static void main(String[] args) {
+    System.out.println("Let's learn about methods");
+    //* what is methods?
+    // ? ans : methods is a block of code which run when it called.
+    // call method
+    sayHey();
+  }
+}
