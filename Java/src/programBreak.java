b5fc312c2f4c8a85915ce90c67bf03e4fac61eaf
Programmer-Jaf <programmerjaf@gmail.com>
2024-12-07 10:18:18 +0600

    java program break through

---
+++ Java/src/programBreak.java
@@ -0,0 +1,15 @@
+public class programBreak {
+  public static void main(String[] args) {
+    /*
+     * public --> public is an access modifier in java
+     * class --> class is a user defined blueprint
+     * programBreak --> it's the name of the public class.It's always same with file name
+     * static --> static allows to call methods without creating classes
+     * void --> void is a return type. It means it return nothing
+     * main --> when a java program start it will always start from the main method
+     * (String[] args) --> it's a command line argument
+     * System.out.println() --> when main method run it will execute as an output in terminal what ever argument it takes.
+     */
+    System.out.println("java program break through");
+  }
+}
