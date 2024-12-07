1e17044a8e58a6358295994438fed6690cb589c8
Programmer-Jaf <programmerjaf@gmail.com>
2024-12-07 10:30:41 +0600

    all about variables in java

---
+++ Java/src/variables.java
@@ -0,0 +1,34 @@
+public class variables {
+  public static void main(String[] args) {
+    System.out.println("learn about variables");
+    // * what are variables ?
+    // ? ans: variables are container to store a value.For example, a water bottle is a variable because it can contains water
+    // byte
+    byte byteValue = 127;
+    System.out.println("The byte value is "+byteValue);
+    // short
+    short shortValue = 32000;
+    System.out.println("The short value is "+shortValue);
+    // int
+    int intValue = 112445644;
+    System.out.println("The int value is "+intValue);
+    // float
+    float floatValue = 1245.1541f;
+    System.out.println("The float value is "+floatValue);
+    // double
+    double doubleValue = 124.054164545;
+    System.out.println("The double value is "+doubleValue);
+    // long
+    long longValue = 124854642324L;
+    System.out.println("The long value is "+longValue);
+    // char
+    char charValue = 'a';
+    System.out.println("The char value is "+charValue);
+    // boolean 
+    boolean booleanValue = true;
+    System.out.println("The boolean value is "+booleanValue);
+    // String
+    String StringValue = "Hello world";
+    System.out.println("The String value is "+StringValue);
+  }
+}
