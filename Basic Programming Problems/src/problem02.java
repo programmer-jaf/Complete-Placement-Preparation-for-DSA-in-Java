20887e13571812e867c0c4dfa4ecf78130276712
Programmer-Jaf <programmerjaf@gmail.com>
2024-12-08 10:43:24 +0600

    solve leap year problem

---
+++ Basic Programming Problems/src/problem02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+// ? Leetcode Link --> https://www.geeksforgeeks.org/problems/leap-year0943/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+public class problem02 {
+	static boolean leapYear(int year){
+		boolean result=false;
+		if (year%400==0) result=true;
+		else if (year%4==0 && year%100!=0) result =true;
+		return result;
+	}
+	public static void main(String[] args) {
+		System.out.println("solve LeapYear problem");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a year");
+		int year = sc.nextInt();
+		boolean result = leapYear(year);
+		System.out.println("The year "+year+" is a leap year is it "+result);
+	}
+}
