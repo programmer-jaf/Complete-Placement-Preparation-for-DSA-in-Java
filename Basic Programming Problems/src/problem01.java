de01e3dc255a8761e412e14eab9b854088c84c92
Programmer-Jaf <programmerjaf@gmail.com>
2024-12-08 10:27:46 +0600

    Solve addDigit With Optimized Solution

--- Basic Programming Problems/src/problem01.java
+++ Basic Programming Problems/src/problem01.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+// ? Leetcode Link --> https://leetcode.com/problems/add-digits/description/
 public class problem01 {
 // ✅ brute-force solution
 	static int addDigit(int num){
@@ -13,13 +13,20 @@ public class problem01 {
 		}
 		return num;
 	}
+	// ✅ Optimized Solution
+	static int addDigitOptimized(int num){
+		if (num==0) return 0;
+		return (num%9==0) ? 9 :(num%9);
+	}
 	public static void main(String[] args) {
 		System.out.println("Add Digit's problem");
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a number : ");
 		int num = sc.nextInt();
 		int result= addDigit(num);
+		int optimizedResult= addDigitOptimized(num);
 		System.out.println("The result is "+result);
+		System.out.println("The optimized result is "+optimizedResult);
 		sc.close();
 	}
 }
